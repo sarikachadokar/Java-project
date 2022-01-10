9f137c14c2579e9f8af00febfb180567fdcd05d0
sarikachadokar <sarikachadhokar@gmail.com>
2022-01-10 22:28:48 +0530

    add files

---
+++ Sampleproj.java
@@ -0,0 +1,11 @@
+package program;
+
+public class Sampleproj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("java is a programming language");
+
+	}
+
+}
